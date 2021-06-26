e8d6d5c470c1d6bdb2059d0aa206cd812b3a6033
zubayerhimel <zhimel007@gmail.com>
2021-06-26 22:54:33 +0600

    vue 3 ref, reactify, watch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="home">
-    <h1>Hello</h1>
+    <input type="text" v-model="search" />
+    <p>Search item - {{ search }}</p>
+    <div v-for="name in matchingName" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref, computed, watch, watchEffect } from "vue";
 export default {
   name: "Home",
+  setup() {
+    const search = ref("");
+    const names = ref(["Mario", "Yoshi", "Luigi", "Toad", "Bower", "Koopa", "Peach"]);
+
+    // watch
+    watch(search, () => {
+      console.log("watching");
+    });
+
+    watchEffect(() => {
+      console.log("Watch effect ran");
+    });
+
+    // functions
+    const matchingName = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+    return { names, search, matchingName };
+  },
 };
 </script>
